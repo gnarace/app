308e4fcbdb392b53b1a62854dd13cf096bbee31e
kr <kr9020@163.com>
2023-11-17 14:28:27 +0800

    1111

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="text-column">
 	<h1>About this app</h1>
-
+	ddfsfsffdsf
 	<p>
 		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
 		following into your command line and following the prompts:
